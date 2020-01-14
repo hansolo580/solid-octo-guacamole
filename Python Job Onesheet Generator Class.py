4b2dcaf7cdeae2608af91564eab7c32620570cfc
hansolo580 <47253266+hansolo580@users.noreply.github.com>
2020-01-14 10:19:40 -0500

    Rewrite
    Rewrite of code for organization.
    Implementation of dropdowns.
    Finally fixed pulling .get calls from TK Entry

--- Python Job Onesheet Generator Class.py
+++ Python Job Onesheet Generator Class.py
@@ -3,12 +3,6 @@ from tkinter import *
 from datetime import datetime
 from datetime import date
 
-#todo
-#rewrite as class
-#in-house vs external version
-#can we use lists and zips to create multiple jobs? each item i.e. jobtitle gets its own list, then these are zipped into job lists? 
-#may make more sense to create directly into ind lists @create screen.
-
 #Style
 colorPrimary = '#AD2623'
 colorSecondary = '#003352'
@@ -17,114 +11,111 @@ colorSecondary = '#003352'
 #Window Rules
 winMain = Tk()
 winMain.title("Onesheet Generator")
-winMain.geometry('800x600')
+winMain.geometry('1000x750')
 #Title
 lblTitle = Label(winMain, text="Onesheet Generator", font=("Arial Bold", 25))
 lblTitle.grid(column=1, row=0)
 
-class addJob:
-    def __init__(self, jobTitle, client, orderDate, startDate, payRate, heavyLifter, shift, hours, location, supervisor, openings, description, education, skills, experience, certifications):
-        self.jobTitle = jobTitle
-        self.client = client
-        self.orderDate = orderDate
-        self.startDate = startDate
-        self.payRate = payRate
-        self.heavyLifter = heavyLifter
-        self.shift = shift
-        self.hours = hours
-        self.location = location
-        self.supervisor = supervisor
-        self.openings = openings
-        self.description = description
-        self.education = education
-        self.skills = skills
-        self.experience = experience
-        self.certifications = certifications
+Label(winMain, text="Job Title").grid(column = 0, row = 2)
+jobTitle = Entry(winMain)
+jobTitle.grid(column = 1, row = 2, columnspan = 3)
 
-    def clkAddJob():
-        winAddJob = Toplevel()
-        Label(winAddJob, text="Job Title").grid(column = 0, row = 2)
-        Name = Entry(winAddJob).grid(column = 1, row = 2, columnspan = 3)
+Label(winMain, text="Client").grid(column = 0, row = 3)
+client = Entry(winMain)
+client.grid(column = 1, row = 3, columnspan = 3)
 
-        Label(winAddJob, text="Client").grid(column = 0, row = 3)
-        Client = Entry(winAddJob).grid(column = 1, row = 3, columnspan = 3)
+Label(winMain, text="Order Date").grid(column = 0, row = 4)
+orderDate = Entry(winMain)
+orderDate.grid(column = 1, row = 4, columnspan = 3)
 
-        Label(winAddJob, text="Order Date").grid(column = 0, row = 4)
-        OrderDate = Entry(winAddJob).grid(column = 1, row = 4, columnspan = 3)
+Label(winMain, text="Start Date").grid(column = 0, row = 5)
+startDate = Entry(winMain)
+startDate.grid(column = 1, row = 5, columnspan = 3)
 
-        Label(winAddJob, text="Start Date").grid(column = 0, row = 5)
-        StartDate = Entry(winAddJob).grid(column = 1, row = 5, columnspan = 3)
+Label(winMain, text="Pay Rate").grid (column = 0, row = 6)
+payRate = Entry(winMain)
+payRate.grid(column = 1, row = 6, columnspan = 3)
 
-        Label(winAddJob, text="Pay Rate").grid (column = 0, row = 6)
-        PayRate = Entry(winAddJob).grid(column = 1, row = 6, columnspan = 3)
-        
+Label(winMain, text="Heavy Lifter?").grid (column = 0, row = 7)
+heavyLifter = StringVar(winMain)
+heavyLifterOptions = {"Yes": True, "No": False}
+heavyLifterSelect = OptionMenu(winMain, heavyLifter, *heavyLifterOptions.keys())
+heavyLifterSelect.grid (column = 1, row = 7)
+#to get - heavyLifterState = heavyLifterOptions[heavyLifter.get()]
 
-        Label(winAddJob, text="Heavy Lifter?").grid (column = 0, row = 7)
-        HeavyLifter = Radiobutton(winAddJob, text="yes", value="yes").grid (column = 1, row = 7)
-        HeavyLifterNo = Radiobutton(winAddJob, text="no", value="no").grid (column = 3, row = 7)
+Label(winMain, text="Shift").grid (column = 0, row = 8)
+shift = StringVar(winMain)
+shiftOptions = {"First": 1, "Second": 2, "Third": 3}
+shiftSelect = OptionMenu(winMain, shift, *shiftOptions.keys())
+shiftSelect.grid (column = 1, row = 8)
 
-        Label(winAddJob, text="Shift").grid (column = 0, row = 8)
-        Shift1 = Radiobutton(winAddJob, text="1", value="1").grid (column = 1, row = 8)
-        Shift2 = Radiobutton(winAddJob, text="2", value="2").grid (column = 2, row = 8)
-        Shift3 = Radiobutton(winAddJob, text="3", value="3").grid (column = 3, row = 8)
+Label(winMain, text="Hours").grid (column = 0, row = 9)
+hours = Entry(winMain)
+hours.grid(column = 1, row = 9, columnspan = 3)
 
-        Label(winAddJob, text="Hours").grid (column = 0, row = 9)
-        Hours = Entry(winAddJob).grid(column = 1, row = 9, columnspan = 3)
+Label(winMain, text="Location").grid (column = 0, row = 10)
+location = Entry(winMain)
+location.grid(column = 1, row = 10, columnspan = 3)
 
-        Label(winAddJob, text="Location").grid (column = 0, row = 10)
-        Location = Entry(winAddJob).grid(column = 1, row = 10, columnspan = 3)
+Label(winMain, text="Supervisor").grid (column = 0, row = 11)
+supervisor = Entry(winMain)
+supervisor.grid(column = 1, row = 11, columnspan = 3)
 
-        Label(winAddJob, text="Supervisor").grid (column = 0, row = 11)
-        Supervisor = Entry(winAddJob).grid(column = 1, row = 11, columnspan = 3)
+Label(winMain, text="Number of Openings").grid (column = 0, row = 12)
+openings = Entry(winMain)
+openings.grid(column = 1, row = 12, columnspan = 3)
 
-        Label(winAddJob, text="Number of Openings").grid (column = 0, row = 12)
-        Openings = Entry(winAddJob).grid(column = 1, row = 12, columnspan = 3)
+#SPACERS
+Label(winMain, text="   ").grid(column = 4, row=1)
+Label(winMain, text="   ").grid(column = 6, row=1)
+Label(winMain, text="").grid(column = 5, row = 13)
+Label(winMain, text="").grid(column = 0, row = 1)
+#ENDSPACERS
 
-        #SPACERS
-        Label(winAddJob, text="   ").grid(column = 4, row=1)
-        Label(winAddJob, text="   ").grid(column = 6, row=1)
-        Label(winAddJob, text="").grid(column = 5, row = 13)
-        Label(winAddJob, text="").grid(column = 0, row = 1)
-        #ENDSPACERS
-        Label(winAddJob, text="Job Description").grid (column = 5, row = 1)
-        JobDescription = Text(winAddJob, width = 30, height = 30).grid(column = 5, row = 2, rowspan = 11)
+Label(winMain, text="Job Description").grid (column = 5, row = 1)
+jobDescription = Text(winMain, width = 30, height = 30)
+jobDescription.grid(column = 5, row = 2, rowspan = 11)
 
-        Label(winAddJob, text="Education").grid (column = 7, row = 1)
-        Education = Text(winAddJob, width = 30, height = 5).grid(column = 7, row = 2, rowspan = 2)
+Label(winMain, text="Education").grid (column = 7, row = 1)
+education = Text(winMain, width = 30, height = 5)
+education.grid(column = 7, row = 2, rowspan = 2)
 
-        Label(winAddJob, text="Experience").grid (column = 7, row = 4)
-        Experience = Text(winAddJob, width = 30, height = 5).grid(column = 7, row = 5, rowspan = 2)
+Label(winMain, text="Experience").grid (column = 7, row = 4)
+experience = Text(winMain, width = 30, height = 5)
+experience.grid(column = 7, row = 5, rowspan = 2)
 
-        Label(winAddJob, text="Skills").grid (column = 7, row = 7)
-        Skills = Text(winAddJob, width = 30, height = 5).grid(column = 7, row = 8, rowspan = 2)
+Label(winMain, text="Skills").grid (column = 7, row = 7)
+skills = Text(winMain, width = 30, height = 5)
+skills.grid(column = 7, row = 8, rowspan = 2)
 
-        Label(winAddJob, text="Certifications").grid (column = 7, row = 10)
-        Certifications = Text(winAddJob, width = 30, height = 5).grid(column = 7, row = 11, rowspan = 2)
+Label(winMain, text="Certifications").grid (column = 7, row = 10)
+certifications = Text(winMain, width = 30, height = 5)
+certifications.grid(column = 7, row = 11, rowspan = 2)
 
-        def clkGenerateOnesheet(jobTitle, client, orderDate, startDate, payRate, heavyLifter, shift, hours, location, supervisor, openings, description, education, skills, experience, certifications):
-            
+def clkGenerateOneSheet():
+    jobTitleValue = jobTitle.get()
+    clientValue = client.get()
+    orderDateValue = orderDate.get()
+    startDateValue = startDate.get()
+    payRateValue = payRate.get()
+    heavyLifterValue = heavyLifterOptions[heavyLifter.get()]
+    shiftSelectValue = shiftOptions[shift.get()]
+    hoursValue = hours.get()
+    locationValue = location.get()
+    supervisorValue = supervisor.get()
+    openingsValue = openings.get()
 
 
-            onesheet = Document()
 
-            onesheet.add_heading(jobTitle, 0)
+    onesheet = Document()
 
-            #pulls current datetime, converts to string to use in filename (prevents duplicate files if loading multiple jobs)
-            
-            today = date.today()
-            onesheet.save(str(client)+str(jobTitle)+str(today)+'.docx')
+    onesheet.add_heading(jobTitleValue, 0)
 
-        Button(winAddJob, text="Generate Onesheet", height = 2, width = 20, bg=colorPrimary, fg="white", command=clkGenerateOnesheet(Name, Client, OrderDate, StartDate, PayRate, HeavyLifter, Shift1, Hours, Location, Supervisor, Openings, JobDescription, Education, Experience, Skills, Certifications)).grid (column = 5, row = 14)
+    today = date.today()
+    onesheet.save(str(clientValue)+str(jobTitleValue)+str(today)+'.docx')
 
-        
-        winAddJob.title("Add New Job")
-        winAddJob.geometry('800x600')
+Button(winMain, text="Generate Onesheet", height = 2, width = 20, bg = colorPrimary, fg="white", command=clkGenerateOneSheet).grid (column = 5, row = 14)
 
-    
 
-    
-    btnAddJob = Button(winMain, text="Add Job", bg=colorPrimary, fg="white", command=clkAddJob)
-    btnAddJob.grid(column=0, row=3)
-    btnAddJob.grid()
 
 winMain.mainloop()
---
+++ rewrite.py
@@ -0,0 +1,121 @@
+from docx import *
+from tkinter import *
+from datetime import datetime
+from datetime import date
+
+#Style
+colorPrimary = '#AD2623'
+colorSecondary = '#003352'
+#End Style
+
+#Window Rules
+winMain = Tk()
+winMain.title("Onesheet Generator")
+winMain.geometry('1000x750')
+#Title
+lblTitle = Label(winMain, text="Onesheet Generator", font=("Arial Bold", 25))
+lblTitle.grid(column=1, row=0)
+
+Label(winMain, text="Job Title").grid(column = 0, row = 2)
+jobTitle = Entry(winMain)
+jobTitle.grid(column = 1, row = 2, columnspan = 3)
+
+Label(winMain, text="Client").grid(column = 0, row = 3)
+client = Entry(winMain)
+client.grid(column = 1, row = 3, columnspan = 3)
+
+Label(winMain, text="Order Date").grid(column = 0, row = 4)
+orderDate = Entry(winMain)
+orderDate.grid(column = 1, row = 4, columnspan = 3)
+
+Label(winMain, text="Start Date").grid(column = 0, row = 5)
+startDate = Entry(winMain)
+startDate.grid(column = 1, row = 5, columnspan = 3)
+
+Label(winMain, text="Pay Rate").grid (column = 0, row = 6)
+payRate = Entry(winMain)
+payRate.grid(column = 1, row = 6, columnspan = 3)
+
+Label(winMain, text="Heavy Lifter?").grid (column = 0, row = 7)
+heavyLifter = StringVar(winMain)
+heavyLifterOptions = {"Yes": True, "No": False}
+heavyLifterSelect = OptionMenu(winMain, heavyLifter, *heavyLifterOptions.keys())
+heavyLifterSelect.grid (column = 1, row = 7)
+#to get - heavyLifterState = heavyLifterOptions[heavyLifter.get()]
+
+Label(winMain, text="Shift").grid (column = 0, row = 8)
+shift = StringVar(winMain)
+shiftOptions = {"First": 1, "Second": 2, "Third": 3}
+shiftSelect = OptionMenu(winMain, shift, *shiftOptions.keys())
+shiftSelect.grid (column = 1, row = 8)
+
+Label(winMain, text="Hours").grid (column = 0, row = 9)
+hours = Entry(winMain)
+hours.grid(column = 1, row = 9, columnspan = 3)
+
+Label(winMain, text="Location").grid (column = 0, row = 10)
+location = Entry(winMain)
+location.grid(column = 1, row = 10, columnspan = 3)
+
+Label(winMain, text="Supervisor").grid (column = 0, row = 11)
+supervisor = Entry(winMain)
+supervisor.grid(column = 1, row = 11, columnspan = 3)
+
+Label(winMain, text="Number of Openings").grid (column = 0, row = 12)
+openings = Entry(winMain)
+openings.grid(column = 1, row = 12, columnspan = 3)
+
+#SPACERS
+Label(winMain, text="   ").grid(column = 4, row=1)
+Label(winMain, text="   ").grid(column = 6, row=1)
+Label(winMain, text="").grid(column = 5, row = 13)
+Label(winMain, text="").grid(column = 0, row = 1)
+#ENDSPACERS
+
+Label(winMain, text="Job Description").grid (column = 5, row = 1)
+jobDescription = Text(winMain, width = 30, height = 30)
+jobDescription.grid(column = 5, row = 2, rowspan = 11)
+
+Label(winMain, text="Education").grid (column = 7, row = 1)
+education = Text(winMain, width = 30, height = 5)
+education.grid(column = 7, row = 2, rowspan = 2)
+
+Label(winMain, text="Experience").grid (column = 7, row = 4)
+experience = Text(winMain, width = 30, height = 5)
+experience.grid(column = 7, row = 5, rowspan = 2)
+
+Label(winMain, text="Skills").grid (column = 7, row = 7)
+skills = Text(winMain, width = 30, height = 5)
+skills.grid(column = 7, row = 8, rowspan = 2)
+
+Label(winMain, text="Certifications").grid (column = 7, row = 10)
+certifications = Text(winMain, width = 30, height = 5)
+certifications.grid(column = 7, row = 11, rowspan = 2)
+
+def clkGenerateOneSheet():
+    jobTitleValue = jobTitle.get()
+    clientValue = client.get()
+    orderDateValue = orderDate.get()
+    startDateValue = startDate.get()
+    payRateValue = payRate.get()
+    heavyLifterValue = heavyLifterOptions[heavyLifter.get()]
+    shiftSelectValue = shiftOptions[shift.get()]
+    hoursValue = hours.get()
+    locationValue = location.get()
+    supervisorValue = supervisor.get()
+    openingsValue = openings.get()
+
+
+
+    onesheet = Document()
+
+    onesheet.add_heading(jobTitleValue, 0)
+
+    today = date.today()
+    onesheet.save(str(clientValue)+str(jobTitleValue)+str(today)+'.docx')
+
+Button(winMain, text="Generate Onesheet", height = 2, width = 20, bg = colorPrimary, fg="white", command=clkGenerateOneSheet).grid (column = 5, row = 14)
+
+
+
+winMain.mainloop()
